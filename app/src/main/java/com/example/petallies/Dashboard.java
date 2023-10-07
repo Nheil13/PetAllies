da560b5d89820c778cc894d445c78656321b5933
Andreu <orongannheil3@gmail.com>
2023-10-07 12:51:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/petallies/Dashboard.java
@@ -0,0 +1,19 @@
+package com.example.petallies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Dashboard extends AppCompatActivity {
+
+    private ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+
+
+    }
+}
