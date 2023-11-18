050e11d478faf2de72b3d3c33116a7d86e890a03
Nheil13 <95905981+Nheil13@users.noreply.github.com>
2023-11-18 12:28:18 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -4,10 +4,11 @@ $server="localhost";
 $username="root";
 $password="";
 $database = "register_from_android";
-// Create connection
 $conn = new mysqli($server, $username, $password, $database);
+
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: ". $conn->connect_error);
+
 }
 ?>
